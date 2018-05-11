e212b5bd0be2c3e34345f1a1ad6d9d6ff25ce824
Mavi <mavi@linkita.net>
2018-05-11 16:51:28 +0200

    Add phpspec

---
+++ tests/spec/FizzBuzzSpec.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace spec\Dojo;
+
+use Dojo\FizzBuzz;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class FizzBuzzSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(FizzBuzz::class);
+    }
+}
